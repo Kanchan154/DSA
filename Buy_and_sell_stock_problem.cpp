7a1870f583f1127b031ebbede8789987ead65bea
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2025-08-20 21:16:53 +0530

    Add files via upload

---
+++ Buy_and_sell_stock_problem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void MaximizeProfit(vector<int> price)
+{
+    int maxProfit = 0, bestBuy = price[0];
+    for (int i = 1; i < price.size(); i++)
+    {
+        if (price[i] > bestBuy)
+        {
+            maxProfit = max(maxProfit, (price[i] - bestBuy));
+        }
+        bestBuy = min(bestBuy, price[i]);
+    }
+    cout << "the best time to buy stock is : " << bestBuy << " with maximum profit : " << maxProfit <<  endl;
+}
+
+int main()
+{ 
+    vector<int> vec = {7, 1, 5, 3, 6, 4};
+    MaximizeProfit(vec);
+}
