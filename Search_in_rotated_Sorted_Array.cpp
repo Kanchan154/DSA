9f1cb836ca2d9726594573168185ece3b4459b83
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2025-08-30 20:49:17 +0530

    Adding Search in rotated Search array

---
+++ Search_in_rotated_Sorted_Array.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void rotated_Sorted_Array(vector<int> array)
+{
+    int start = 0, end = array.size() - 1;
+    int mid, found = false, target;
+    cout << "Enter the target value: ";
+    cin >> target;
+
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        if (array[mid] == target)
+        {
+            cout << endl
+                 << "Element found at index: " << mid;
+            found = true;
+            break;
+        }
+        else if (array[start] <= array[mid])
+        { // left sorting
+            if (array[start] < target && array[mid] > target)
+            { // search in left
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        else if (array[start] >= array[mid])
+        { // search in right
+            if (array[mid] < target && array[end] > target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Element not found...." << endl;
+    }
+}
+
+int main()
+{
+    vector<int> array = {3, 4, 5, 6, 7, 0, 1, 2};
+    rotated_Sorted_Array(array);
+
+    return 0;
+}
