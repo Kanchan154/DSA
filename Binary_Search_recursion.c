8525d1c31e204238b16742bffa505df27c06cb08
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2026-06-02 23:29:13 +0530

    ...

--- Binary_Search_recursion.c
+++ Binary_Search_recursion.c
@@ -3,18 +3,14 @@
 int binarySearch(int arr[], int low, int high, int key) {
     if (high >= low) {
         int mid = low + (high - low) / 2;
-
         if (arr[mid] == key) {
             return mid;
         }
-
         if (arr[mid] > key) {
             return binarySearch(arr, low, mid - 1, key);
         }
-
         return binarySearch(arr, mid + 1, high, key);
     }
-
     return -1;
 }
 
